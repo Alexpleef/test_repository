8bfd9c59af5e60255d2299691693cd69ffe6c6a2
Alexpleef <karpusalex87@gmail.com>
2019-07-15 10:14:02 +0300

    Task 14 version 2.0

--- task13/Service/Interface/EntityManagerInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-require '/var/www/html/task13/Service/dbMethods.php';
-interface EntityManagerInterface
-{
-    public function getById($id);
-    public function save($id, $title, $text);
-}
